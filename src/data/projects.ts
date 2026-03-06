ad2c1fc7b413306ef6838d9eb1b7a425a7686efd
Changpeng Dao <cx.benance@gmail.com>
2026-03-07 00:34:11 +0700

    Add Benance Bird game (#20)
    * Added Benance Bird Game logo
    
    Benance bird game logo
    
    * Update projects.ts (Added benance bird game)
    
    Play to earn and burn LUNC gaming on Terra classic blockchain
    
    * Revise syntax comma
    
    * remove trailing slash and rename to benance_bird.png
    
    * Delete public/logos/entertainment/Benance_Bird.png
    
    * rename to benance_bird.png

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -881,5 +881,13 @@ export const projects: ProjectLink[] = [
     indicator: 'onchain',
     logo: '/public/logos/entertainment/genesisraid.png',
     categories: ['entertainment']
-  }
+  },
+  {
+    name: "Benance Bird",
+    url: "https://benance-bird-mainnet.vercel.app",
+    description: "Play-to-Earn and Burn arcade game on Terra Classic",
+    indicator: "onchain",
+    logo: "/public/logos/entertainment/benance_bird.png",
+    categories: ["entertainment"]
+}
 ];
